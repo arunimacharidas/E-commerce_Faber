95ad18f1784de49d1920c0f7fc4715881ea827a4
arunima <arunimaaruchembakapali@gmail.com>
2023-10-11 15:21:10 +0530

    adminside

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const {getsignin,getsignup,gettable,getindex,getwidget,gettypography,getform,getelement,getchart,getbutton,getblank,get404}=require('../controllers/admincontroller');
 
-/* GET users listing. */
-router.get('/')
+router.get('/signin',getsignin)
+router.get('/signup',getsignup)
+router.get('/table',gettable)
+router.get('/index',getindex)
+router.get('/widget',getwidget)
+router.get('/typography',gettypography)
+router.get('/form',getform)
+router.get('/element',getelement)
+router.get('/chart',getchart)
+router.get('/button',getbutton)
+router.get('/blank',getblank)
+router.get('/404',get404)
 
 
 
